db203668a60f7e6758374924a92c81354622e200
Chris <chris@saganic.co.uk>
2022-09-15 10:57:07 +0100

    fix: 404

---
+++ src/pages/[...all].vue
@@ -0,0 +1,19 @@
+<template>
+  <div w-full h-full flex flex-col justify-center items-center space-y-12>
+    <img alt="Wildpoint logo" src="/logo-light.svg" w-80>
+    <div flex flex-col text-center space-y-2>
+      <h1 text-white text-3xl font-semibold>
+        Wildpoint is closed for Winter 2022
+      </h1>
+      <h2 text-white text-2xl>
+        We'll be back in Spring 2023
+      </h2>
+    </div>
+    <p class="text-center text-white/90">
+      A massive <span font-bold>thank you</span> to everyone that has supported us since our inception. We are <span
+        font-bold
+      >extremely grateful</span>.
+    </p>
+  </div>
+</template>
+
