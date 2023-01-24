7f59acbcde0cd5cca9dccf1e346c9255240221c4
Chris <chris@saganic.co.uk>
2023-01-24 10:37:01 +0000

    feat: new closing message

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,42 @@
 <template>
   <div w-full h-full flex flex-col justify-center items-center space-y-12>
     <img alt="Wildpoint logo" src="/logo-light.svg" w-80>
-    <div flex flex-col text-center space-y-2>
-      <h1 text-white text-3xl font-semibold>
-        Wildpoint is closed for Winter 2022
-      </h1>
-      <h2 text-white text-2xl>
-        We'll be back in Spring 2023
-      </h2>
+    <div max-w-2xl flex flex-col space-y-2>
+      <div flex flex-col space-y-2>
+        <p text-white>
+          Dear valued customers,
+        </p>
+        <p text-white>
+          It’s with deep regret that we inform you Wildpoint LTD is no longer operational and has decided to shut down as of
+          23rd
+          December 2022.
+        </p>
+        <p text-white>
+          We want to thank you so much for your support. We set out to create a business that could help everyone get back to
+          nature and find their wild and we know that many people had enjoyable trips thanks to the wonderful hosts on our
+          platform.
+        </p>
+        <p text-white>
+          All customer & host data has been deleted, and the business has officially been shut down.
+        </p>
+        <p text-white>
+          If you’re looking for a camping stay in the UK we recommend checking
+          <a href="https://hipcamp.com" underline>
+            Hipcamp.com
+          </a>
+        </p>
+        <p text-white>
+          Thank you so much for all your support and your custom.
+        </p>
+        <p text-white>
+          For enquiries please email - <a href="mailto:hello@wildpoint.com" underline>hello@wildpoint.com</a>
+        </p>
+        <p text-white>
+          Kindest regards, <br>
+          Wildpoint
+        </p>
+      </div>
     </div>
-    <p class="text-center text-white/90">
-      A massive <span font-bold>thank you</span> to everyone that has supported us since our inception. We are <span font-bold>extremely grateful</span>.
-    </p>
   </div>
 </template>
 
